26ca610860210bb222aa238c7f8eb33eb328e10b
Rushi57 <rushihanzo07@gmail.com>
2026-09-07 14:27:41 +0800

    MainMenu
    SafeAreaMainMenu

---
+++ Assets/Code&Script/SafeArea.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeArea : MonoBehaviour
+{
+    RectTransform rectTransform;
+    Rect lastSafeArea = new Rect(0, 0, 0, 0);
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        Refresh();
+    }
+
+    void Update()
+    {
+        Refresh();
+    }
+
+    void Refresh()
+    {
+        Rect safeArea = Screen.safeArea;
+
+        if (safeArea != lastSafeArea)
+        {
+            lastSafeArea = safeArea;
+            ApplySafeArea(safeArea);
+        }
+    }
+
+    void ApplySafeArea(Rect r)
+    {
+        Vector2 anchorMin = r.position;
+        Vector2 anchorMax = r.position + r.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        rectTransform.anchorMin = anchorMin;
+        rectTransform.anchorMax = anchorMax;
+    }
+}
